fec7f0a26f2f48ac1e1cba84a01e99f48230807b
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-07-12 14:26:12 +0300

    ++ ???

--- compiler/fir/resolve/testData/resolve/stdlib/recursiveBug.kt
+++ compiler/fir/resolve/testData/resolve/stdlib/recursiveBug.kt
@@ -1,3 +1,11 @@
 class Foo(name: () -> String) {
-    val name = run { name() }.length
+    val result = run { name() }
+
+    val name = result.length
+}
+
+fun bar(name: () -> String) {
+    val result = run { name() }
+
+    val name = result.length
 }
